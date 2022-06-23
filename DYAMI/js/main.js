db9f247c51b8a803cd6dd5291b3c3d9b1363bd13
shape2ee <shape1026@gmail.com>
2022-06-23 10:25:34 +0900

    [220623] header 영역 수정

--- DYAMI/js/main.js
+++ DYAMI/js/main.js
@@ -20,17 +20,30 @@ const moveTop = () => {
 }
 
 window.addEventListener("scroll", scrollCheck);
-goToTop.addEventListener("click", moveTop)
+goToTop.addEventListener("click", moveTop);
 
 /***************************************/
 
-const header = document.querySelector("header")
+const header = document.querySelector("header");
+const aList = header.getElementsByTagName("a");
 
 const bgChange = () => {
+    // header의 배경색 흰색으로 바꾸기
     if(window.pageYOffset > 0) {
-        header.classList.add("header--white")
+        header.classList.add("header--white");
     } else {
-        header.classList.remove("header--white")
+        header.classList.remove("header--white");
+    }
+
+    // 스크롤 확인 후 일정부분 지나면 padding 줄이는 클래스 붙이기
+    if(window.pageYOffset > 200) {
+        for(let i = 0; i < aList.length; i++){
+            aList[i].classList.add("a-move");
+        }
+    } else {
+        for(let i = 0; i < aList.length; i++) {
+            aList[i].classList.remove("a-move");
+        }
     }
 }
 
